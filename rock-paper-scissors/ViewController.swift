2511f87ebcc05b6fb6e2cba17af759a4ebcffef3
Azat IOS <ios.azatech@Azat-Air.connectify>
2017-11-29 13:29:54 +0200

    Enjoy The game

--- rock-paper-scissors/ViewController.swift
+++ rock-paper-scissors/ViewController.swift
@@ -7,19 +7,134 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var opponentLabel: UILabel!
+    @IBOutlet weak var myScore: UILabel!
+    @IBOutlet weak var opponentsScore: UILabel!
+    @IBOutlet weak var chosenLbl: UILabel!
+
+    // vars for score labels
+    var me = 0
+    var opponent = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // beginning the app
+        myScore.text = String(me)
+        opponentsScore.text = String(opponent)
+        opponentLabel.text = "Choose something!"
+        
+    }
+    // random number
+    var a = Int()
+    var randomNumber = Int()
+    func randomChoice() {
+        randomNumber = Int(arc4random() % 3 )
+        print("randomNumber is \(randomNumber)")
+    }
+    // Round
+    func gameOn() {
+        switch(randomNumber) {
+        case 0:
+            chosenLbl.text = "I chose Rock!"
+            if a == 0 {
+            alertFunction(title: "It's Draw!")
+                me += 1
+                opponent += 1
+                hideLabel()
+            } else {
+                if a == 1 {
+                    alertFunction(title: "You won :)")
+                    opponentLabel.text = "U R LUCKY!"
+                    me += 1
+                }
+                if a == 2 {
+                    alertFunction(title: "You Lost !")
+                    opponentLabel.text = "HA-HA! U LOOSER"
+                    opponent += 1
+                }
+            }
+            break
+        case 1:
+            chosenLbl.text = "I chose Paper!"
+            if a == 0 {
+                alertFunction(title: "You Lost !")
+                opponentLabel.text = "U LOOSER"
+                opponent += 1
+            } else {
+                if a == 1 {
+                    alertFunction(title: "It's Draw!")
+                    hideLabel()
+                    me += 1
+                    opponent += 1
+                }
+                if a == 2 {
+                    alertFunction(title: "You won :)")
+                    opponentLabel.text = "NOT MY DAY"
+                    me += 1
+                }
+            }
+            break
+        case 2:
+            chosenLbl.text = "I chose Scissors!"
+            if a == 0 {
+                alertFunction(title: "You won :)")
+                opponentLabel.text = "NOT MY DAY"
+                me += 1
+            } else {
+                if a == 1 {
+                    alertFunction(title: "You Lost!")
+                    opponentLabel.text = "I AM AWESOME"
+                    opponent += 1
+                }
+                if a == 2 {
+                    alertFunction(title: "It's Draw!")
+                    hideLabel()
+                    me += 1
+                    opponent += 1
+                }
+            }
+            break default: break
+        }
+    }
+    func hideLabel() {
+        opponentLabel.isHidden = true
+    }
+    func unHideLabel(){
+        opponentLabel.isHidden = false
+    }
+    // Update score Labels
+    func rightScore() {
+        opponentsScore.text = String(opponent)
+        myScore.text = String(me)
+    }
+    // Alert function with result
+    func alertFunction(title: String){
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Play again", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    // Apps logic
+    func allCycle(chooseInt: Int) {
+        a = chooseInt
+        randomChoice()
+        unHideLabel()
+        gameOn()
+        rightScore()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func rockActionBtn(_ sender: UIButton) {
+        allCycle(chooseInt: 0)
     }
-
-
+    @IBAction func paperActionBtn(_ sender: UIButton) {
+        allCycle(chooseInt: 1)
+    }
+    @IBAction func scissorsActionBtn(_ sender: UIButton) {
+        allCycle(chooseInt: 2)
+    }
 }
-
